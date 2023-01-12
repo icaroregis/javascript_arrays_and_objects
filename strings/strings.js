983253dd06f7e01f95561f7396f20da830d63ead
icaro_regis <progalmeidaregis@outlook.com>
2023-01-12 14:56:44 -0300

    strings, variáveis

---
+++ strings/strings.js
@@ -0,0 +1,3 @@
+const numberOne = Number(prompt('Digite um número:'));
+const numberTwo = Number(prompt('Digite outro número:'));
+const resultado = alert(`O resultado da soma é: ${numberOne + numberTwo}`);
