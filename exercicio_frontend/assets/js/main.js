5920855f6d21c59f1981e8a93f79ce92aba2ebe7
icaro_regis <progalmeidaregis@outlook.com>
2023-01-17 17:41:13 -0300

    criação de função para para mostrar mensagem com IMC

--- exercicio_frontend/assets/js/main.js
+++ exercicio_frontend/assets/js/main.js
@@ -24,34 +24,42 @@
 
   function Imc(result) {
     if (result < 18.5) {
-      return (resultado.innerHTML += `<p>Abaixo do Peso</p>`);
+      return exibirMensagem('Abaixo do Peso', result);
     }
 
     if (result >= 18.5 && result < 24.9) {
-      return (resultado.innerHTML += `<p>Peso Normal</p>`);
+      return exibirMensagem('Peso Normal', result);
     }
 
     if (result >= 25 && result < 29.9) {
-      return (resultado.innerHTML += `<p>Sobrepeso</p>`);
+      return exibirMensagem('Sobrepeso', result);
     }
 
     if (result >= 30 && result < 34.9) {
-      return (resultado.innerHTML += `<p>Obsesidade Grau I</p>`);
+      return exibirMensagem('Obsesidade Grau I', result);
     }
 
     if (result >= 35 && result < 39.9) {
-      return (resultado.innerHTML += `<p>Obsesidade Grau II</p>`);
+      return exibirMensagem('Obsesidade Grau II', result);
     }
 
     if (result > 40) {
-      return (resultado.innerHTML += `<p>Obsesidade Grau III</p>`);
+      return exibirMensagem('Obsesidade Grau III', result);
     }
 
-    return (resultado.innerHTML += `<p>Altura ou peso inválido</p>`);
+    return exibirMensagem('Altura ou peso inválido', result);
   }
 
   function limpar(peso, altura) {
     peso.value = '';
     altura.value = '';
+  }
+
+  function exibirMensagem(msg, result) {
+    resultado.innerHTML = '';
+    resultado.innerHTML += `<p>Seu imc é de: ${result}. ${msg}</p>`;
+    setTimeout(() => {
+      resultado.innerHTML = '';
+    }, 3000);
   }
 })();
