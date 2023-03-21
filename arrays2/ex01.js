32074d1f059a049587e1c70cd11c62f322e4df1e
icaro_regis <progalmeidaregis@outlook.com>
2023-03-21 20:21:09 -0300

    dom

--- arrays2/ex01.js
+++ arrays2/ex01.js
@@ -5,7 +5,7 @@ let qtdeDeConvidados = Number(prompt('Digite o número de convidados'));
 
 for (let i = 0; i < qtdeDeConvidados; i++) { 
    //1 forma: nomes[i] = prompt(`Digite o nome do convidado ${i}`);  
-  nomes.push(prompt(`Digite o nome do convidado`));     
+  nomes.push(prompt(`Digite o nome do convidado`));      
 }
 
 console.log(nomes)     
